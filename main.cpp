52f442ea0fe9f0a242411496d297bb151486f703
Kenny Claka <kennyigbechi@gmail.com>
2019-01-20 10:29:45 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+double  square(double x){
+    return x*x;
+}
+
+void print_square(double x){
+    cout<<"The square of " << x << " is " << square(x) <<endl;
+}
+
+int main() {
+    print_square(56);
+}*/
+
+
+int main(){
+    vector <int> kenny = {3, 4, 2, 4, 9, 4, 2,};
+
+    vector<int>
+
+    cout<<kenny[3];
+}
